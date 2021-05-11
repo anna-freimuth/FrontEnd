260a070d87905ffadcdb253b79d3f754cb7ab1be
Anna Freimuth <a.freimuth90@gmail.com>
2021-05-11 19:50:20 +0200

    likes update

--- 2021_05_11_data_flow/src/LikesRating.js
+++ 2021_05_11_data_flow/src/LikesRating.js
@@ -3,7 +3,7 @@ import React from  "react";
 export default({id, rating, likesHandle, dislikesHandle,dislikes,likes})=>{
     return(
         <div>
-            <button id="like" onClick={()=>likesHandle(id)}>like {likes}</button>
+            <button id="like" onClick={()=>likesHandle(id)}>likes {likes}</button>
             <button id="rating">rating {rating}</button>
             <button id="dislike" onClick={()=>dislikesHandle(id)}>dislikes {dislikes}</button>
         </div>
